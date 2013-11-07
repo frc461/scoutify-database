92e30baf925ce28d1d476429e80a8f6b1771769f
Kristofer Rye <kristofer.rye@gmail.com>
2013-11-06 19:09:20 -0500

    Adding initial stuffies.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScoutifyDatabase::Application.config.secret_key_base = 'eacb336a52543b0fd7aaed81737babd20cdb8fc4fc764a4fa37378f21c6e7f7473ea97f1bdebd6efcac6778d639582637aa5f84ff5a0c1306cfb6476775f272d'
