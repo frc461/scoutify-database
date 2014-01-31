6e2eb9fe019f2573f78654e8996b83d87403d10f
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-01-31 13:23:36 -0500

    Add function to update teams with info from TBA.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,26 @@
 class Team < ActiveRecord::Base
 
+	require "net/http"
+
 	has_many :records
 	has_and_belongs_to_many :events
+
+	def tba_update(key = nil)
+		key ||= "frc" + number.to_s
+		uri = URI("http://www.thebluealliance.com/api/v1/team/details")
+		params = { team: key }
+		uri.query = URI.encode_www_form(params)
+		res = Net::HTTP.get_response(uri)
+		unless res.is_a?(Net::HTTPSuccess)
+			puts res.uri
+			puts res.code
+			puts res.body
+			raise "Error with TBA API"
+		end
+		json = MultiJson.load(res.body)
+		self.name = json["nickname"]
+		self.number = json["team_number"]
+		self.save
+	end
 
 end
